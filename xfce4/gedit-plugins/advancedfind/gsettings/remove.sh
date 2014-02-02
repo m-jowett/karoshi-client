57424a7aca517fed43cfc7d2f991b61e55aeaec8
Robin McCorkell <rmccorkell@karoshi.org.uk>
2014-02-02 19:20:24 +0000

    Restructure and cleanup
    Moved a few things around:
     * multimedia
        No longer exists, since the `detect-dvd` script that was contained in it
        is an XFCE4 specific thing, and has been merged with the `xfce4` directory
     * firefox, thunderbird
        In a separate directory `applications`
     * xfce4/gedit-plugins
        gedit plugins are not really part of Karoshi, and so have been removed.
     * xfce4/gvfs-mounts
        Moved into two nested directories: xfce4/gvfs/mounts
    
    All scripts have had the Makefile message at the top modified to fit more with
    how autoconf does it (@configure_input@) and how other Makefile-passed
    variables are declared: @makefile_in@

--- xfce4/gedit-plugins/advancedfind/gsettings/remove.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm /usr/share/glib-2.0/schemas/org.gnome.gedit.plugins.advancedfind.gschema.xml
-glib-compile-schemas /usr/share/glib-2.0/schemas/
--- xfce4/gedit-plugins/advancedfind/po/make_pot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#Make pot file.
-
-app_name="advancedfind"
-
-xgettext --output="${app_name}".pot ../*.py ../*.glade
-
-
